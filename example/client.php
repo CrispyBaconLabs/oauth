a63a6ef42160fe4c3d12e5f9eb26812df441dbbd
andyster <andyster@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2007-11-09 06:00:23 +0000

    adding more functions to test client

--- example/client.php
+++ example/client.php
@@ -1,11 +1,12 @@
 <?php
 require_once("common.inc.php");
 
+$test_server = new TestOAuthServer(new MockOAuthStore());
 
 $key = @$_REQUEST['key'];
 $secret = @$_REQUEST['secret'];
-$token = @$_REQUEST['oauth_token'];
-$token_secret = @$_REQUEST['oauth_token_secret'];
+$token = @$_REQUEST['token'];
+$token_secret = @$_REQUEST['token_secret'];
 $endpoint = @$_REQUEST['endpoint'];
 $action = @$_REQUEST['action'];
 
@@ -22,7 +23,19 @@ if ($action == "request_token") {
   $req_req->build_request($test_consumer, NULL);
   $req_req->sign_request_HMAC_SHA1($test_consumer, NULL);
   Header("Location: $req_req");
+} 
+else if ($action == "authorize") {
+  $callback_url = "http://$base_url/client.php?key=$key&secret=$secret&oauth_token=$token&oauth_token_secret=$token_secret&endpoint=" . urlencode($endpoint);
+  $auth_url = $endpoint . "?oauth_token=$token&oauth_callback=".urlencode($callback_url);
+  Header("Location: $auth_url");
 }
+else if ($action == "access_token") {
+  $acc_req = new OAuthRequest(array(), "GET", $endpoint);
+  $acc_req->build_request($test_consumer, $test_token);
+  $acc_req->sign_request_HMAC_SHA1($test_consumer, $test_token);
+  Header("Location: $acc_req");
+}
+
 
 
 $acc_token = new OAuthConsumer("accesskey", "accesssecret", 1);
@@ -57,6 +70,12 @@ consumer key: <input type="text" name="key" value="<?php echo $key; ?>" /><br />
 consumer secret: <input type="text" name="secret" value="<?php echo $secret;?>" /><br />
 make a token request (don't forget to copy down the values you get)
 <input type="submit" name="action" value="request_token" />
+<h3>Enter Your Request Token / Secret</h3>
+token: <input type="text" name="token" value="<?php echo $token; ?>" /><br />
+token secret: <input type="text" name="token_secret" value="<?php echo $token_secret; ?>" /><br />
+<p><strong>Don't forget to update your endpoint</strong></p>
+try to authorize this token: <input type="submit" name="action" value="authorize" /><br />
+try to get an access token: <input type="submit" name="action" value="access_token" /><br />
 
 <h3>Currently Supported Signature Methods</h3>
 <ul>
