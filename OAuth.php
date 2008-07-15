e1e05476b3e70381d6ff551f2daa7f0795ae463f
andyster <andyster@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2008-07-15 22:55:34 +0000

    added basestring output to rsa-sha1 sig method

--- OAuth.php
+++ OAuth.php
@@ -123,6 +123,7 @@ class OAuthSignatureMethod_RSA_SHA1 extends OAuthSignatureMethod {/*{{{*/
 
   public function build_signature(&$request, $consumer, $token) {/*{{{*/
     $base_string = $request->get_signature_base_string();
+    $request->base_string = $base_string;
   
     // Fetch the private key cert based on the request
     $cert = $this->fetch_private_cert($request);
