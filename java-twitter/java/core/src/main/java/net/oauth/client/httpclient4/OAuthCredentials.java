80b2c09e7bb0c835bfc9fcfde394cc83784fd211
jmkristian <jmkristian@f7ae4463-c52f-0410-a7dc-93bad6e629e8>
2009-04-21 21:31:17 +0000

    branched from trunk -r963

---
+++ java-twitter/java/core/src/main/java/net/oauth/client/httpclient4/OAuthCredentials.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2009 Paul Austin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.oauth.client.httpclient4;
+
+import net.oauth.OAuthAccessor;
+import org.apache.http.auth.UsernamePasswordCredentials;
+
+/**
+ * An OAuthAccessor, to be used as credentials for an AuthScheme based on OAuth.
+ * The OAuthAccessor is contained by reference, so you can change it to contain
+ * the OAuth tokens and secrets that you receive from a service provider.
+ * 
+ * @author John Kristian
+ */
+public class OAuthCredentials extends UsernamePasswordCredentials {
+
+    private final OAuthAccessor accessor;
+
+    public OAuthCredentials(OAuthAccessor accessor) {
+        super(accessor.consumer.consumerKey, accessor.consumer.consumerSecret);
+        this.accessor = accessor;
+    }
+
+    public OAuthAccessor getAccessor() {
+        return accessor;
+    }
+
+    /** Get the current consumer secret, to be used as a password. */
+    @Override
+    public String getPassword() {
+        return getAccessor().consumer.consumerSecret;
+    }
+
+    /** Get the current consumer key, to be used as a password. */
+    @Override
+    public String getUserName() {
+        return getAccessor().consumer.consumerKey;
+    }
+
+}
---
+++ java-twitter/java/core/src/test/java/net/oauth/OAuthValidatorTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2008 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.oauth;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Dirk Balfanz
+ * @author John Kristian
+ */
+public class OAuthValidatorTest extends TestCase {
+
+    private long currentTime;
+    private SimpleOAuthValidator validator;
+
+    @Override
+    protected void setUp() throws Exception {
+        currentTime = (System.currentTimeMillis() / 1000) * 1000;
+        validator = new SimpleOAuthValidator() {
+            @Override
+            protected long currentTimeMsec() {return currentTime;}
+        };
+    }
+
+    public void testDuplicateParameters() throws Exception {
+        validator.checkSingleParameters(new OAuthMessage("", "", OAuth.decodeForm("x=y&x=y")));
+        String parameters = "oauth_version=1.0&oauth_version=1.0";
+        OAuthMessage msg = new OAuthMessage("", "", OAuth.decodeForm(parameters));
+        try {
+            validator.checkSingleParameters(msg);
+            fail("repeated parameter");
+        } catch (OAuthProblemException expected) {
+            assertEquals(OAuth.Problems.PARAMETER_REJECTED, expected.getProblem());
+            assertEquals(parameters, expected.getParameters().get(OAuth.Problems.OAUTH_PARAMETERS_REJECTED));
+        }
+    }
+
+    public void testSimpleOAuthValidator() throws Exception {
+        final long window = SimpleOAuthValidator.DEFAULT_TIMESTAMP_WINDOW;
+        tryTimestamp(currentTime - window - 500); // round up
+        tryTimestamp(currentTime + window + 499); // round down
+        try {
+            tryTimestamp(currentTime - window - 501);
+            fail("validator should have rejected timestamp, but didn't");
+        } catch (OAuthProblemException expected) {
+        }
+        try {
+            tryTimestamp(currentTime + window + 500);
+            fail("validator should have rejected timestamp, but didn't");
+        } catch (OAuthProblemException expected) {
+        }
+
+        tryVersion(1.0);
+        try {
+            tryVersion(0.9);
+            fail("validator should have rejected version, but didn't");
+        } catch (OAuthProblemException expected) {
+        }
+        try {
+            tryVersion(1.2);
+            fail("validator should have rejected version, but didn't");
+        } catch (OAuthProblemException expected) {
+        }
+        try {
+            tryVersion(2.0);
+            fail("validator should have rejected version, but didn't");
+        } catch (OAuthProblemException expected) {
+        }
+    }
+
+    private void tryTimestamp(long timestamp) throws Exception {
+        OAuthMessage msg = new OAuthMessage("", "", OAuth.newList(
+                "oauth_timestamp", ((timestamp + 500) / 1000) + "",
+                "oauth_nonce", "lsfksdklfjfg"));
+        validator.validateTimestampAndNonce(msg);
+    }
+
+    private void tryVersion(double version) throws Exception {
+        OAuthMessage msg = new OAuthMessage("", "", OAuth.newList(
+                "oauth_version", version + ""));
+        validator.validateVersion(msg);
+    }
+
+}
